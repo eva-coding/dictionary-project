22714128e8dffd78273301ae9d1fe616193e1709
Eva Machova <eva.machova1997@gmail.com>
2025-05-14 17:25:05 +0100

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header>Hello</header>
+      <div className="container">
+        <header className="App-header">Hello</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Eva Machova</small>
+        </footer>
+      </div>
     </div>
   );
 }
