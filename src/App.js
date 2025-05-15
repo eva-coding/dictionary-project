6960770277a1a733e57b79d61fe4691a04981e18
Eva Machova <eva.machova1997@gmail.com>
2025-05-15 17:22:04 +0100

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -10,7 +10,12 @@ function App() {
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by Eva Machova</small>
+          <small>
+            Coded by Eva Machova, hosted on Netlify, code available on{" "}
+            <a href="https://github.com/eva-coding/dictionary-project">
+              GitHub
+            </a>
+          </small>
         </footer>
       </div>
     </div>
