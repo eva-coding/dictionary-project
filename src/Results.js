77859ea070f86b9b17097db3606520add6bc4621
Eva Machova <eva.machova1997@gmail.com>
2025-05-22 18:45:50 +0100

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -2,11 +2,15 @@ import React from "react";
 import Meaning from "./Meaning";
 
 export default function Result(props) {
-  console.log(props.results);
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        {props.results.phonetic && (
+          <div className="Phonetic">
+            <span>/{props.results.phonetic}/</span>
+          </div>
+        )}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
