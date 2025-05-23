82ae8f7f137e97749ed50004e7f2d4d3171472ab
Eva Machova <eva.machova1997@gmail.com>
2025-05-23 17:09:06 +0100

    Added a header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import logo from "./logo.png";
 import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
         <main>
           <Dictionary defaultKeyword="coffee" />
         </main>
